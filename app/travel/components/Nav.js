4a34c2e804f229da2fca4dfb7a1dac42a59be783
devneolee <devneolee@gmail.com>
2023-09-29 19:33:01 -0700

    update logo image

--- app/travel/components/Nav.js
+++ app/travel/components/Nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
     <div className="nav">
       <Link href="/travel">
         <div className="logo">
-          <div className="logoImage"><img src="/travelerBlue.png" width="40px" height="65px"/></div>
+          <div className="logoImage"><img src="/travelerDarkgray.png" width="40px" height="65px"/></div>
           <div><h1 className="logoKorean" >현지여행정보</h1></div>
         </div>
       </Link>
