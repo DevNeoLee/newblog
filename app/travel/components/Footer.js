f9780913cb116f6f48ca73194c32b8f7b5b2fd9e
devneolee <devneolee@gmail.com>
2024-04-01 12:32:29 +0900

    change footer class to className

--- app/travel/components/Footer.js
+++ app/travel/components/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer class="footer pt-4 my-md-5 pt-md-5 border-top">
-      <div class="row">
-        <div class="col-6 col-md">
+    <footer className="footer pt-4 my-md-5 pt-md-5 border-top">
+      <div className="row">
+        <div className="col-6 col-md">
           <ul>
-            <li><div class="text-muted mt-2 mb-4 d-flex"><a href="https://newblog-beta.vercel.app/travel"><span class="mr-1">© 뭐여행 </span></a></div></li>
-            <li class="mb-4"><a target="_blank" href="https://newblog-beta.vercel.app/sitemap.xml" ><span class="mr-2">Sitemap</span><i class="fab fa-github fa-lg"></i></a></li>      								       
-            <li class="mt-2 mb-4"><a target="_blank" href="https://unsplash.com/photos/man-taking-photo-of-hot-air-balloons-eOcyhe5-9sQ"><span class="mr-2">Photo by Mesut Kaya from Unsplash</span><i class="fab fa-linkedin fa-lg"></i></a></li>
+            <li><div className="text-muted mt-2 mb-4 d-flex"><a href="https://newblog-beta.vercel.app/travel"><span className="mr-1">© 뭐여행 </span></a></div></li>
+            <li className="mb-4"><a target="_blank" href="https://newblog-beta.vercel.app/sitemap.xml" ><span className="mr-2">Sitemap</span><i className="fab fa-github fa-lg"></i></a></li>      								       
+            <li className="mt-2 mb-4"><a target="_blank" href="https://unsplash.com/photos/man-taking-photo-of-hot-air-balloons-eOcyhe5-9sQ"><span className="mr-2">Photo by Mesut Kaya from Unsplash</span><i className="fab fa-linkedin fa-lg"></i></a></li>
           </ul>
         </div>
       </div>
