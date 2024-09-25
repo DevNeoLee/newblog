dd9e66d20e7e73f64afc6b41071536fd6fff9498
DevNeoLee <devneolee@gmail.com>
2024-09-25 00:30:36 -0700

    add home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,70 @@
-import Link from 'next/link'
 
-export default function page() {
+import Link from 'next/link'
+import { getMetadata, getCatalogue, getKorean } from './travel/utils/getData';
+import fs from 'fs'
+import Image from 'next/image';
+import FooterMain from './travel/components/FooterMain';
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      // Generate a random index
+      const j = Math.floor(Math.random() * (i + 1));
+      
+      // Swap elements at indices i and j
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export default function Home() {
+  const catalogues = getCatalogue();
+
+  const metaInfo = getMetadata("나를찾는세계여행");
+  
   return (
-    <div className="homeContainer">
-      <h1>생활의 지혜, 현명한 블로그 웹진</h1>
-      <Link href="/travel"><h2>- 세계여행 현지 꿀팁 모음집</h2></Link>
-    </div>
+    <>
+      <div className="travelMenu">
+        <Link href="/" className="logo" style={{ textDecoration: "none"}}>
+          <div className="logoCompass" >
+              <Image 
+                priority
+                alt="main menu icon"
+                src="/compass.png"
+                width="30"
+                height="34"
+              />
+          </div>
+          <div className="logoKorean" id="blur" >MOYAHUG</div>
+        </Link>
+        <Link href={`travel/`} className="countryLink" > 
+          <div id="blur" className="country">여행과 나</div>
+        </Link>
+      </div>
+      <div className="travelMain">
+        {/* <Image 
+          priority
+          alt="travel main image"
+          src="/background_image_mesut-kaya.jpg"
+          fill
+          quality={60}
+          style={{ zIndex: "-12", position: "absolute", objectFit: "cover"}}
+        /> */}
+        <div className="mainPagePrologues">
+          {shuffleArray(metaInfo).map((post, id) => (
+            <Link href={`travel/prologue/${post.link}`} key={id} className="countryCardLink">
+              <div className="countryCard" key={id}>
+                {/* <div className="countryImage" style={{ backgroundColor: post.color}}></div> */}
+                <h2 className="countryTitle" >{ post.title.length > 60 ? post.title.slice(0, 60) + "..." : post.title} </h2>
+                <p className="countryDate" >{post.date}</p>
+                <div className="countryParagraph" >
+                  <p className="countrySubtitle" >{post.subtitle}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <FooterMain />
+    </>
   )
 }
---
+++ app/travel/components/FooterMain.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function FooterMain() {
+  return (
+    <footer>
+      <div className="row">
+        <Link href="/" className="logo" style={{ textDecoration: "none"}}>
+            <div className="logoCompass" >
+              <Image 
+                priority
+                alt="main menu icon"
+                src="/compass.png"
+                width="30"
+                height="34"
+              />
+            </div>
+             <div className="logoKorean" id="blur" >MOYAHUG</div>
+        </Link>
+        <div className="attribution"><div></div></div>
+       
+      </div>
+      <div className="copyright">
+        <p style={{ color: "gray"}}>© Copyright {new Date().getFullYear()} <strong style={{ color: "black", margin: "0 1rem"}}> MOYAHUG</strong> All rights reserved.</p> 
+        {/* <p><strong>PCB</strong> is owned and operated by <strong>Pacific Customs Brokers Ltd., Pacific Customs Brokers Inc.</strong>, and <strong>PCB Freight Management Ltd.</strong></p> */}
+      </div>
+    </footer>    
+  )
+}
